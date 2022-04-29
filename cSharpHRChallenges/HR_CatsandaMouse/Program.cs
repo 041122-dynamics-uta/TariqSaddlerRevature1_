24033196b721c267f325b00baaf15a85adf2cda4
TariqSaddlerRevature <Tariq658@revature.net>
2022-04-29 05:06:41 -0400

    All the latest CSharp HackerRank Challenges completed!

---
+++ cSharpHRChallenges/HR_CatsandaMouse/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace HR_CatsandaMouse
+{
+    class Program
+    {
+        static string catAndMouse(int x, int y, int z)
+        {
+            int ACatDistance = Math.Abs(x - z);
+            int BCatDistance = Math.Abs(y - z);
+            
+            if(ACatDistance < BCatDistance)
+            {
+                return "Cat A";
+            }
+            else if(ACatDistance > BCatDistance)
+            {
+                return "Cat B";
+            }
+            else
+            {
+                return "Mouse C";
+            }
+        }
+
+        static void Main(string[] args) 
+        {
+            TextWriter textWriter = new StreamWriter(("c:\\Users\\Tariq Saddler\\RevatureFiles\\TariqSaddlerRevature1_\\cSharpHRChallenges\\HR_CatsandaMouse\\OUTPUT.txt"), true);
+
+            int q = Convert.ToInt32(Console.ReadLine());
+
+            for (int qItr = 0; qItr < q; qItr++) 
+            {
+                string[] xyz = Console.ReadLine().Split(' ');
+
+                int x = Convert.ToInt32(xyz[0]);
+
+                int y = Convert.ToInt32(xyz[1]);
+
+                int z = Convert.ToInt32(xyz[2]);
+
+                string result = catAndMouse(x, y, z);
+
+                textWriter.WriteLine(result);
+            }
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
---
+++ cSharpHRChallenges/HR_TimeConversion/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace HR_TimeConversion
+{
+    class Result
+{
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string timeConversion(string s)
+    {
+        
+        int theHour = Int32.Parse(s.Substring(0, 2));
+        int INTmilitaryHour = theHour + 12;
+        string STRmilitaryHour = INTmilitaryHour.ToString();
+        string militaryTime = s.Substring(2, 6);
+
+        if(s.Substring(8) == "AM" && s.Substring(0, 2) == "12")
+        {
+            militaryTime = militaryTime.Insert(0, "00");  
+        }
+        
+        else if(s.Substring(8) == "PM" && s.Substring(0, 2) != "12")
+        {
+            if(s.Substring(0, 1) == "0")
+            {
+                theHour = Int32.Parse(s.Substring(1, 1));
+                INTmilitaryHour = theHour + 12;
+                STRmilitaryHour = INTmilitaryHour.ToString();
+            }
+            if(theHour < 12)
+            {
+                militaryTime = militaryTime.Insert(0, STRmilitaryHour);
+            }
+        }
+        
+        else
+        {
+            militaryTime = militaryTime.Insert(0, s.Substring(0, 2));
+        }
+        
+        return militaryTime;
+    }
+
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(("c:\\Users\\Tariq Saddler\\RevatureFiles\\TariqSaddlerRevature1_\\cSharpHRChallenges\\HR_TimeConversion\\OUTPUT.txt"), true);
+
+            string s = Console.ReadLine();
+
+            string result = Result.timeConversion(s);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
